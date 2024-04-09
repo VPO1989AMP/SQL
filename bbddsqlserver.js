30115e36a77f3abd3023aa338234c0c127ac73f2
Victor_PB <victorpb@me.com>
2024-04-09 16:03:29 +0200

    ava

--- bbddsqlserver.js
+++ bbddsqlserver.js
@@ -1,4 +1,20 @@
-const mysql = require("mysql8");
-//import {pass} from "./password.js"
 const pass = require('./password.js')
 console.log(pass)
+const mssql = require("mssql")
+
+const config = {
+    user: "sa",
+    password: pass,
+    database:"northwind",
+    host:"localhost",
+    port:1433,
+    pool:{
+        min:0,
+        max:10,
+        idleTimeoutMilis: 30000,
+    },
+    options:{
+        encrypt: true,
+        trustServerCertificates: true
+    }
+}
